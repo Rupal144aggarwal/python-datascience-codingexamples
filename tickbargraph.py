ef6cf2d9ac38e813b34c46880f555747a64bcfef
harsh99code <72193356+harsh99code@users.noreply.github.com>
2020-10-01 14:33:16 +0530

    Update tickbargraph.py

--- tickbargraph.py
+++ tickbargraph.py
@@ -5,8 +5,8 @@ with open('tec.csv')as csvfile:
     for row in readcsv:
         print(row)
         
-x=[1,2,3,4,5,6,7,8,9,11]
-y=[2,4,5,7,11,6,9,12,2,4]
+x=[1,2,3,4,5,6,7,8,9,11,12,16]
+y=[2,4,5,7,11,6,9,12,2,4,14,15]
 tick_label=['one','two','three','our','five','six','seven','two','three','our']
-plt.bar(x,y,tick_label=tick_label,width=0.2,color=['red','green'])
+plt.bar(x,y,tick_label=tick_label,width=0.2,color=['blue','green'])
 plt.show()
